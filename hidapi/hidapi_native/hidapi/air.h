9b133438609651bc2d86f2626bdc84abf080a630
nimrod97@gmail.com <nimrod97@gmail.com@022d6f70-3516-5457-e988-f6750b982e88>
2011-12-30 00:03:55 +0000

    init

--- hidapi/hidapi_native/hidapi/air.h
+++ hidapi/hidapi_native/hidapi/air.h
@@ -9,6 +9,8 @@
 #define LOG_ENABLED false
 
 #ifdef _WIN32
+    //force using mingw <stdint.h>, see FlashRuntimeExtensions.h
+    #undef WIN32
     #include "FlashRuntimeExtensions.h"
     #define EXPORT __declspec(dllexport)
 
