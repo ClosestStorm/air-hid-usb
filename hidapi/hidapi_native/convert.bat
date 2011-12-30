d1e5597f4e40e82cfa4291145fe2cd83d1852192
nimrod97@gmail.com <nimrod97@gmail.com@022d6f70-3516-5457-e988-f6750b982e88>
2011-12-30 15:09:08 +0000

    init

---
+++ hidapi/hidapi_native/convert.bat
@@ -0,0 +1,22 @@
+@echo off
+rem creates mingw compatible library from FlashRuntimeExtensions.lib
+
+set air_def_name=Adobe AIR.def
+set air_lib=%AIR_SDK_HOME%\lib\win\FlashRuntimeExtensions.lib
+set air_a=FlashRuntimeExtensions.dll.a
+
+cd windows
+
+if "%1"=="clean" goto clean
+echo convert "%air_lib%" to "%air_a%"
+reimp -d "%air_lib%"
+dlltool -d "%air_def_name%" -l "%air_a%"
+goto quit
+
+:clean
+echo clean
+if exist "%air_a%" echo delete "%air_a%" && del "%air_a%"
+if exist "%air_def_name%" echo delete "%air_def_name%" && del "%air_def_name%"
+goto quit
+
+:quit
